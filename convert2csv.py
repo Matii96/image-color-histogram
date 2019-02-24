ce469f6e57d91c32c2721be74184b6bd49c29b1e
Matii96 <mateusz.fonfara96@gmail.com>
2019-02-24 17:40:38 +0100

    Typo fix

--- convert2csv.py
+++ convert2csv.py
@@ -18,7 +18,7 @@ def main():
     print('Loading image from '+ sys.argv[1])
     img = cv2.imread(sys.argv[1])
 
-    #Define structure for .csv format
+    #Convert img to csv
     pixels_countX = len(img)
     pixels_countY = len(img[0])
     result = {
@@ -29,26 +29,6 @@ def main():
         'g': img[:,:,1].ravel(),
         'b': img[:,:,2].ravel()
     }
-
-    #Convert img to csv
-    '''
-    pixels_countX = len(img)
-    pixels_countY = len(img[0])
-    pixels_count_total = pixels_countX * pixels_countY
-    print('Conversion started')
-    for x in range(pixels_countX):
-        for y in range(pixels_countY):
-            pixel = img[x][y]
-            result['x'].append(x)
-            result['y'].append(y)
-            result['z'].append(0)
-            result['r'].append(pixel[0])
-            result['g'].append(pixel[1])
-            result['b'].append(pixel[2])
-
-            progress = (x * pixels_countY + y) / pixels_count_total * 100
-            print('%.2f%%\r' % progress, end='')
-    '''
 
     #Save result
     print('Saving to '+ config['convert2csv_output'])
