d47613d55c382debf543f748b0a4a6b3d4790a2b
Matii96 <mateusz.fonfara96@gmail.com>
2019-02-25 22:14:44 +0100

    Bugfix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import time
 import multiprocessing as mp
 
 def count_colors(procnum, dict, img_part):
-    print('Thread %d started' % procnum)
+    print('Thread %d started' % (procnum+1))
     colors = np.array([[[0] * 256] * 256] * 256)
     for row in img_part:
         colors[row[0]][row[1]][row[2]] += 1
@@ -33,16 +33,16 @@ def main():
 
     #Even distribution of tasks to threads
     img_length = len(img)
-    thread_tasks = [img_length // config['threads']] * config['threads']
+    thread_tasks = [img_length // config['threads']] * min(config['threads'], img_length)
     for i in range(img_length % config['threads']):
         thread_tasks[i] += 1
 
     threads = []
     last_task = 0
-    threads_to_run = range(min(config['threads'], len(thread_tasks)))
-    print('Creating %d thread(s)' % config['threads'])
+    threads_to_run = len(thread_tasks)
+    print('Creating %d thread(s)' % threads_to_run)
     beginning = time()
-    for i in threads_to_run:
+    for i in range(threads_to_run):
         row_end = last_task + thread_tasks[i]
         thread = mp.Process(target=count_colors, args=(i, dict, img[last_task:row_end]))
         thread.start()
@@ -50,7 +50,7 @@ def main():
         last_task = row_end
 
     result = np.array([[[0] * 256] * 256] * 256)
-    for i in threads_to_run:
+    for i in range(threads_to_run):
         threads[i].join()
         print('Thread %d ended' % i)
         result = np.add(result, dict[i])
@@ -65,7 +65,7 @@ def main():
         'b': np.tile(np.arange(0, 256), 256 * 256),
         'count': result.ravel()
     }
-    
+
     #Save result
     print('Saving to '+ config['main_output'])
     df = pd.DataFrame(result_csv)
